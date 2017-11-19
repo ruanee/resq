55a31a1c28c447519eeddad2ffdd68eb4be14f07
xg <xg@xg-PC>
2017-11-19 12:51:49 +0800

    improve login

--- reques/routes/users.js
+++ reques/routes/users.js
@@ -4,7 +4,7 @@ const db = require('../db')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	db.query2("select row_number() OVER(ORDER BY user_name) seq,user_name username,status,mod_date from public.users where active='T' and status ='Active'", [], function(error, rows) {
+	db.query2("select row_number() OVER(ORDER BY user_name) seq,user_name username,status,to_char(mod_date,'yyyy-MM-dd') mod_date from public.users where active='T' and status ='Active'", [], function(error, rows) {
 		res.render('users', { title: 'Users' , rows: rows});
 	})
 //	res.sendFile('weiui.html')
