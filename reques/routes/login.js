1307c7030277e372ad373b314c34b0b5c9be9157
xg <xg@xg-PC>
2017-12-08 22:06:06 +0800

    improve login

--- reques/routes/login.js
+++ reques/routes/login.js
@@ -15,31 +15,35 @@ router.get('/logout', function(req, res, next) {
 });
 router.post('/', function(req, res, next) {
 	var uname = req.body.username;
-	try {
-		var sid= req.sessionID, store = req.sessionStore;
-		if(sid && store) {
-			var sessions = store.sessions, times = 0;
-			for ( var p in sessions) {
-				var ses = JSON.parse(sessions[p]);
-				if(sid != p && ses.user == uname && ses.sessionId) {
-					times++;
-				}
-			}
-			if(times >= 1 && uname !='test') {
-				res.render('login', { title: '登录', message:"您的账号已经在别的地方登陆了!"});
-				return;
-			}
-		}
-	} catch(e) {
-		
-	}
-	
+	var dback = {title: '登录'};
 	const hash = crypto.createHmac('sha256', req.body.password).update(globals.hashKey).digest('hex');
 //	console.log(hash);
 	if(uname && globals.userData && globals.userData[uname] && globals.userData[uname]["password"] == hash) {
+		
+		// protect only one user per login
+		try {
+			var sid= req.sessionID, store = req.sessionStore;
+			if(sid && store) {
+				var sessions = store.sessions, times = 0;
+				for ( var p in sessions) {
+					var ses = JSON.parse(sessions[p]);
+					if(sid != p && ses.user == uname && ses.sessionId) {
+						times++;
+					}
+				}
+				if(times >= 1 && uname !='test') {
+					dback.message = "您的账号已经在别的地方登陆了!";
+					res.render('login', dback);
+					return;
+				}
+			}
+		} catch(e) {
+			
+		}
+		
 		req.session.user = uname;
 		req.session.sessionId=log.guid();
-		var refer = req.session.referer;
+//		var refer = req.session.referer;
 //		if(refer && refer.indexOf("css.map") == -1 && refer.indexOf("favicon") == -1) {
 //			res.redirect(req.session.referer);
 //		} else {
@@ -47,6 +51,7 @@ router.post('/', function(req, res, next) {
 //		}
 		return;
 	}
-	res.render('login', { title: '登录', message:"用户名或者密码错误!"});
+	dback.message = "用户名或者密码错误!";
+	res.render('login', dback);
 });
 module.exports = router;
