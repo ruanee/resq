56db1b2fd4b797effa1e3592377114e331652af3
xg <xg@xg-PC>
2018-01-14 11:29:29 +0800

    aaa

--- reques/globals.js
+++ reques/globals.js
@@ -4,7 +4,7 @@ var Globals = {
 	outputFolder: 'F:/work/FE/B2B/test/',
 	hashKey:'miaomiao',
 	userData: null,
-	menus: [{type:'专生本',title:'Networking'},{type:'Computer Networking',title:'计算机网络'},{type:'Software Enginerring',title:'软件工程'},{type:'Java',title:'基础习题'},
+	menus: [{type:'专升本',title:'Networking'},{type:'Computer Networking',title:'计算机网络'},{type:'Software Enginerring',title:'软件工程'},{type:'Java',title:'基础习题'},
 		{type:'sql',title:'Oracle'},{type:'SSH',title:'SSH'},{type:'NTC',title:'NTC'}],
 	users: {
 			  'admin':'pass@123','test':'123'
